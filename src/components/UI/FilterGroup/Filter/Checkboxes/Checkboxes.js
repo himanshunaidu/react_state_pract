c698cecaba754dbd8cc4e6daea894beba10daf33
Himanshu Naidu <himanshunaidu12161997@gmail.com>
2021-11-09 18:57:52 +0530

    Layout adjustment

--- src/components/UI/FilterGroup/Filter/Checkboxes/Checkboxes.js
+++ src/components/UI/FilterGroup/Filter/Checkboxes/Checkboxes.js
@@ -1,23 +1,31 @@
 import React, { Fragment } from "react";
 import { Container } from "react-bootstrap";
 
+import styles from "./Checkboxes.module.css";
+
 // import filters from "../../../../../data/filters";
 
 const Checkboxes = (props) => {
   return (
-    <>
+    <Container className={styles.container}>
+      <h3>{props.filter_obj.title}</h3>
       {
         //props.filter_obj
         props.filter_obj.filters.map((filter) => {
           return (
-            <Container key={filter.filter_id}>
-              <input type="checkbox" />
-              <label>{filter.filter_name}</label>
-            </Container>
+            <div className={styles.input_container} key={filter.filter_id}>
+              <input
+                type="checkbox"
+                id={filter.filter_id}
+                name={filter.filter_id}
+                value={filter.filter_id}
+              />
+              <label htmlFor={filter.filter_id}>{filter.filter_name}</label>
+            </div>
           );
         })
       }
-    </>
+    </Container>
   );
 };
 
