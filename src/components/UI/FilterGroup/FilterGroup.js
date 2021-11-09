b923ed8efda418468113224d206fbddb8297f2d5
Himanshu Naidu <himanshunaidu12161997@gmail.com>
2021-11-10 00:20:53 +0530

    Block page code

--- src/components/UI/FilterGroup/FilterGroup.js
+++ src/components/UI/FilterGroup/FilterGroup.js
@@ -1,16 +1,18 @@
 import React, { useState } from "react";
+import { Container } from "react-bootstrap";
 
+import styles from "./FilterGroup.module.css";
 import filters from "../../../data/filters";
 import Filter from "./Filter/Filter";
 
 const FilterGroup = (props) => {
   const filter_view = (
-    <>
+    <Container className={styles.container}>
       {Object.keys(filters).map((filter_key) => {
         const filter_obj = filters[filter_key];
         return <Filter key={filter_obj.id} filter_obj={filter_obj} />;
       })}
-    </>
+    </Container>
   );
 
   return filter_view;
--- src/pages/Block/Block.js
+++ src/pages/Block/Block.js
@@ -1,14 +1,20 @@
 import React, { Fragment, useState, useReducer } from "react";
+import { Container } from "react-bootstrap";
 
+import styles from "./Block.module.css";
 import FilterGroup from "../../components/UI/FilterGroup/FilterGroup";
 import CardsDisplay from "../../components/UI/CardsDisplay/CardsDisplay";
 
 const Block = (props) => {
   return (
-    <>
-      <FilterGroup></FilterGroup>
-      <CardsDisplay></CardsDisplay>
-    </>
+    <Container className={styles.container} fluid>
+      <div style={{ gridColumn: "first / second" }}>
+        <FilterGroup></FilterGroup>
+      </div>
+      <div style={{ gridColumn: "second / auto" }}>
+        <CardsDisplay></CardsDisplay>
+      </div>
+    </Container>
   );
 };
 
