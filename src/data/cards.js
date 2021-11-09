157f34183ccb9bca6bdc084b20491109ba1fc7bd
Himanshu Naidu <himanshunaidu12161997@gmail.com>
2021-11-09 17:58:42 +0530

    Filters

--- src/data/cards.js
+++ src/data/cards.js
@@ -1,9 +1,39 @@
 const cardData = [
-  { id: "1", title: "Card 1", text: "This is the first card" },
-  { id: "2", title: "Card 2", text: "This is the second card" },
-  { id: "3", title: "Card 3", text: "This is the third card" },
-  { id: "4", title: "Card 4", text: "This is the fourth card" },
-  { id: "5", title: "Card 5", text: "This is the fifth card" },
+  {
+    id: "1",
+    title: "Card 1",
+    text: "This is the first card",
+    version: 1,
+    lang: 1,
+  },
+  {
+    id: "2",
+    title: "Card 2",
+    text: "C'est la deuxième carte",
+    version: 1,
+    lang: 2,
+  },
+  {
+    id: "3",
+    title: "Card 3",
+    text: "This is the third card",
+    version: 2,
+    lang: 1,
+  },
+  {
+    id: "4",
+    title: "Card 4",
+    text: "C'est la quatrième carte",
+    version: 1,
+    lang: 2,
+  },
+  {
+    id: "5",
+    title: "Card 5",
+    text: "This is the fifth card",
+    version: 2,
+    lang: 1,
+  },
 ];
 
 export default cardData;
---
+++ src/data/filters.js
@@ -0,0 +1,17 @@
+const versions = {
+  title: "Version",
+  filters: [
+    { filter_id: 1, filter_name: 1 },
+    { filter_id: 2, filter_name: 2 },
+  ],
+};
+
+const languages = {
+  title: "Language",
+  filters: [
+    { filter_id: 1, filter_name: "English" },
+    { filter_id: 2, filter_name: "French" },
+  ],
+};
+
+export default { versions: versions, languages: languages };
