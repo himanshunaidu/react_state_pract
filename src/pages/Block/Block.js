9d5ad63c0fa8483f055378ff316ace3a23cf5319
Himanshu Naidu <himanshunaidu12161997@gmail.com>
2021-11-10 19:17:37 +0530

    FilterGroup updates 1

--- src/pages/Block/Block.js
+++ src/pages/Block/Block.js
@@ -6,8 +6,20 @@ import FilterGroup from "../../components/UI/FilterGroup/FilterGroup";
 import CardsDisplay from "../../components/UI/CardsDisplay/CardsDisplay";
 import Pagination from "../../components/UI/Pagination/Pagination";
 import cardData from "../../data/cards";
+import filters from "../../data/filters";
+
+const initializeSelectedFilters = () => {
+  const selectedFilters = {};
+  Object.keys(filters).forEach((key) => {
+    selectedFilters[key] = new Set();
+  });
+  return selectedFilters;
+};
 
 const Block = (props) => {
+  const [selectedFilters, setSelectedFilters] = useState(
+    initializeSelectedFilters()
+  );
   const [activePage, setActivePage] = useState(1);
 
   const selectionChanged = useCallback((page) => {
